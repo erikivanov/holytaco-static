b1cf3c21561221080df626996a7c234be63e0154
Erik Ivanov <hallo@erik-ivanov.de>
2022-08-15 09:21:09 +0200

    last changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 var msnry = new Masonry( '.menu-wrapper', {
     itemSelector: '.menu-item'
-});  
+});
+
+const swiper = new Swiper('.swiper', {
+    loop: false,
+    slidesPerView: 'auto',
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+$('a[href*="#"]:not([href="#"])').click(function() {
+    var offset = -80;
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top + offset
+            }, 500);
+            return false;
+        }
+    }
+});
